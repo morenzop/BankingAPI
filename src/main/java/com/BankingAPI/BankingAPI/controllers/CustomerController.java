1d2ac11e210d6cd1bf036e322f3496c001ec5225
Sarah <sarahhargreaves10@gmail.com>
2019-11-07 21:38:35 -0500

    depricated withdrawal and deposit services using methods in repositories, @restcontroller on customer controller

--- src/main/java/com/BankingAPI/BankingAPI/controllers/CustomerController.java
+++ src/main/java/com/BankingAPI/BankingAPI/controllers/CustomerController.java
@@ -4,9 +4,11 @@ import com.BankingAPI.BankingAPI.models.Customer;
 import com.BankingAPI.BankingAPI.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 public class CustomerController {
 
     @Autowired
--- src/main/java/com/BankingAPI/BankingAPI/repositories/DepositsRepository.java
+++ src/main/java/com/BankingAPI/BankingAPI/repositories/DepositsRepository.java
@@ -4,8 +4,10 @@ import com.BankingAPI.BankingAPI.models.Deposit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DepositsRepository extends JpaRepository<Deposit, Long> {
 
-    Iterable<Deposit> getDepositsByPayeeId(String account);
+    List<Deposit> findAllByPayeeId(String payeeId);
 }

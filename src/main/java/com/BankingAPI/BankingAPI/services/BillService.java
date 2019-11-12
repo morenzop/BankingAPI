68ac244d255c57a252a035d6391266a35ed2a976
Ray Felton <frajeon@gmail.com>
2019-11-12 14:56:30 -0500

    'ray'

--- src/main/java/com/BankingAPI/BankingAPI/services/BillService.java
+++ src/main/java/com/BankingAPI/BankingAPI/services/BillService.java
@@ -1,6 +1,7 @@
 package com.BankingAPI.BankingAPI.services;
 
 import com.BankingAPI.BankingAPI.Bill;
+import com.BankingAPI.BankingAPI.models.Bill;
 import com.BankingAPI.BankingAPI.repositories.BillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
